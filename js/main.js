f9c877af602b54455c078f0e36afffa07adc7edb
bvasilop <bvasilop@gmail.com>
2017-05-10 18:53:50 -0700

    Added required validation for order, name , zip

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 /* Custom JS goes here. */
 
-// For this assignment you'll need to do a few things:
+    // For this assignment you'll need to do a few things:
 // 1. Create a document ready handler.
 // 2. Define a validation object for use on your page.
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+$(document).on( 'ready' , function() {
+
+  $('#order-form').validate({
+      submitHandler: function(form) {
+            form.submit();
+       },
+
+      rules: {
+            'your-name': {
+              required: true,
+              maxlength: 128
+       },
+
+        'your-zip':{
+              required: true,
+              minlength: 5,
+              digits: true
+      },
+        'card-number': {
+          required: true,
+          creditcard: true
+        }
+
+    }
+
+  });
+});
